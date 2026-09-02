2656ea229b6de6dfcae8b9c66952efdadee0858b
rav-blue <86786625+rav-blue@users.noreply.github.com>
2026-09-02 16:58:23 -0500

    Set base path for deployment in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base: '/Ravenna-Bluejays-PWA/'
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
