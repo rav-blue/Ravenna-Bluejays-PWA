f147b90bacad511cf9c74edf0af1d75d10494793
rav-blue <86786625+rav-blue@users.noreply.github.com>
2026-09-02 17:04:43 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: './',
     plugins: [react(), tailwindcss()],
-    base: '/Ravenna-Bluejays-PWA/'
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
